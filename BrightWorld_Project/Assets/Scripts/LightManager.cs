aba35d2b31fb3c65f968bd39f3a8e602f49f7e4d
Jon Shoberg <shobe028@umn.edu>
2016-08-07 00:17:23 -0500

    Added light manager and scene switcher. Player and enemies persist between scenes
    I did a thing and it worked!

---
+++ BrightWorld_Project/Assets/Scripts/LightManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightManager : MonoBehaviour {
+
+	public static double light = 100f;
+	public static bool dark = false;
+
+	// Use this for initialization
+	void Start () {
+
+		if (light <= 50f) 
+		{
+
+			dark = true;
+
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (light <= 50f) 
+		{
+
+			dark = true;
+
+		}
+
+		if (!dark) {
+
+			light -= Time.deltaTime * 3.0;
+		}
+
+	}
+}
---
+++ BrightWorld_Project/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	private static double light;
+	private static bool dark;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		light = LightManager.light;
+		dark = LightManager.dark;
+
+		if (light <= 50f && !dark) {
+			SceneManager.LoadScene (1);
+		}
+		if (light >= 100f && dark) {
+			SceneManager.LoadScene (0);
+		}
+	}
+
+	void Awake(){
+		DontDestroyOnLoad (this);
+	}
+
+}
