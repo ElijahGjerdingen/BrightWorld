ec30754e78116dcaabe3cc3f9ac176e76f6edb60
Jon Shoberg <shobe028@umn.edu>
2016-08-06 19:21:11 -0500

    Added camera control script

---
+++ BrightWorld_Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	GameObject player;
+	public float horizontalFollow;
+	public float verticalFollow;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		player = GameObject.FindGameObjectWithTag("Player");
+		if (player.transform.position.x > transform.position.x)
+		{
+			if (player.transform.position.x > transform.position.x + horizontalFollow)
+			{
+				transform.position = new Vector3(player.transform.position.x - horizontalFollow, transform.position.y, transform.position.z);
+			}
+		}
+		else if (player.transform.position.x < transform.position.x)
+		{
+			if (player.transform.position.x < transform.position.x - horizontalFollow)
+			{
+				transform.position = new Vector3(player.transform.position.x + horizontalFollow, transform.position.y, transform.position.z);
+			}
+		}
+
+		if (player.transform.position.y > transform.position.y)
+		{
+			if (player.transform.position.y > transform.position.y + verticalFollow)
+			{
+				transform.position = new Vector3(transform.position.x, player.transform.position.y - verticalFollow, transform.position.z);
+			}
+		}
+		else if (player.transform.position.y < transform.position.y)
+		{
+			if (player.transform.position.y < transform.position.y - verticalFollow)
+			{
+				transform.position = new Vector3(transform.position.x, player.transform.position.y + verticalFollow, transform.position.z);
+			}
+		}
+
+	}
+
+}
