fdae557280201e7e9b1fcf922bfb2ee2cfd556b2
Elijah Gjerdingen <elijah.gjerdingen@gmail.com>
2016-08-07 11:38:36 -0500

    Darkness

--- BrightWorld_Project/Assets/Scripts/SpriteManagerBlumper.cs
+++ BrightWorld_Project/Assets/Scripts/SpriteManagerBlumper.cs
@@ -6,11 +6,13 @@ public class SpriteManagerBlumper : MonoBehaviour {
 	public Sprite friendlySprite;
 	public Sprite enemySprite;
 	private bool isFriendly;
+    private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		isFriendly = true;
-	}
+        anim = GetComponent<Animator>();
+    }
 
 	void Awake(){
 		DontDestroyOnLoad (this);
@@ -19,12 +21,12 @@ public class SpriteManagerBlumper : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (LightManager.isDark && isFriendly) {
-			GetComponent<SpriteRenderer>().sprite = enemySprite;
-			isFriendly = false;
+            anim.SetTrigger("Dark");
+            isFriendly = false;
 			gameObject.layer = 13;
 		} else {
 			if (!LightManager.isDark && !isFriendly) {
-				GetComponent<SpriteRenderer>().sprite = friendlySprite;
+				anim.SetTrigger("Light");
 				isFriendly = true;
 				gameObject.layer = 9;
 			}
--- BrightWorld_Project/Assets/Scripts/SpriteManagerTallman.cs
+++ BrightWorld_Project/Assets/Scripts/SpriteManagerTallman.cs
@@ -6,26 +6,28 @@ public class SpriteManagerTallman : MonoBehaviour {
 	public Sprite friendlySprite;
 	public Sprite enemySprite;
 	private bool isFriendly;
+    private Animator anim;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		isFriendly = true;
-	}
+        anim = GetComponent<Animator>();
+    }
 
-	void Awake(){
+    void Awake(){
 		DontDestroyOnLoad (this);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (LightManager.isDark && isFriendly) {
-			GetComponent<SpriteRenderer>().sprite = enemySprite;
-			isFriendly = false;
+            anim.SetTrigger("Dark");
+            isFriendly = false;
 			gameObject.layer = 13;
 		} else {
 			if (!LightManager.isDark && !isFriendly) {
-				GetComponent<SpriteRenderer>().sprite = friendlySprite;
-				isFriendly = true;
+                anim.SetTrigger("Light");
+                isFriendly = true;
 				gameObject.layer = 9;
 			}
 		}
