988f889ba3a5d8331fd96f368d9434ee00b0375d
Eli Knott <elijk173@gmail.com>
2016-08-07 07:49:58 -0500

    Damage added

---
+++ BrightWorld_Project/Assets/Scripts/CollideDamage.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CollideDamage : MonoBehaviour {
+
+    private bool cooldown = false;
+    private int time;
+
+    // Use this for initialization
+    void Start () {
+        time = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (cooldown)
+        {
+            time++;
+            if (time == 60)
+            {
+                time = 0;
+                cooldown = false;
+            }
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Dark_Monster") && !cooldown)
+        {
+            LightManager.light -= 10;
+            cooldown = true;
+        }
+    }
+}
