f671dd8ca7b4c49faf4b5a4af9df59f9733de32f
Jon Shoberg <shobe028@umn.edu>
2016-08-07 08:02:07 -0500

    Added code to switch sprites

--- BrightWorld_Project/Assets/Scripts/SpriteManagerPuffirhana.cs
+++ BrightWorld_Project/Assets/Scripts/SpriteManagerPuffirhana.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class SpriteManagerPuffirhana : MonoBehaviour {
 
+	public Sprite friendlySprite;
+	public Sprite enemySprite;
+	private bool isFriendly;
+
 	// Use this for initialization
 	void Start () {
-	
+		isFriendly = true;
 	}
 
 	void Awake(){
@@ -14,6 +18,14 @@ public class SpriteManagerPuffirhana : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (LightManager.isDark && isFriendly) {
+			GetComponent<SpriteRenderer>().sprite = enemySprite;
+			isFriendly = false;
+		} else {
+			if (!LightManager.isDark && !isFriendly) {
+				GetComponent<SpriteRenderer>().sprite = friendlySprite;
+				isFriendly = true;
+			}
+		}
 	}
 }
--- BrightWorld_Project/Assets/Scripts/SpriteManagerTallman.cs
+++ BrightWorld_Project/Assets/Scripts/SpriteManagerTallman.cs
@@ -3,17 +3,29 @@ using System.Collections;
 
 public class SpriteManagerTallman : MonoBehaviour {
 
+	public Sprite friendlySprite;
+	public Sprite enemySprite;
+	private bool isFriendly;
+
 	// Use this for initialization
 	void Start () {
-	
+		isFriendly = true;
 	}
 
 	void Awake(){
 		DontDestroyOnLoad (this);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (LightManager.isDark && isFriendly) {
+			GetComponent<SpriteRenderer>().sprite = enemySprite;
+			isFriendly = false;
+		} else {
+			if (!LightManager.isDark && !isFriendly) {
+				GetComponent<SpriteRenderer>().sprite = friendlySprite;
+				isFriendly = true;
+			}
+		}
 	}
 }
