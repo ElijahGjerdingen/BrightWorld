14a5a47282db0bc961d5ccda88e9499dcd60d1da
Eli Knott <elijk173@gmail.com>
2016-08-07 11:55:35 -0500

    A thing

--- BrightWorld_Project/Assets/Scripts/Killzone.cs
+++ BrightWorld_Project/Assets/Scripts/Killzone.cs
@@ -5,6 +5,9 @@ public class Killzone : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        LightManager.light = 0;
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            LightManager.light = 0;
+        }
     }
 }
