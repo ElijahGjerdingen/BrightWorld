4d1b488b589954d7b6190d669fabf93234cc5404
Elijah Gjerdingen <elijah.gjerdingen@gmail.com>
2016-08-07 11:00:27 -0500

    Music and End Game

---
+++ BrightWorld_Project/Assets/Scripts/EndGameLamp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndGameLamp : MonoBehaviour {
+
+    void OnTriggerStay2D(Collider2D thing)
+    {
+        if (thing.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+    }
