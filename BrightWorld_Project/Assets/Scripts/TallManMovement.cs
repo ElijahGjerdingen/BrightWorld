1cbed5742093ca4dc919bbdb760f687509f57082
Elijah Gjerdingen <elijah.gjerdingen@gmail.com>
2016-08-06 19:38:37 -0500

    Added back tallman movement

--- BrightWorld_Project/Assets/Scripts/TallManMovement.cs
+++ BrightWorld_Project/Assets/Scripts/TallManMovement.cs
@@ -5,7 +5,7 @@ public class TallManMovement : MonoBehaviour {
     private bool leftHit = false;
     private Rigidbody2D rigidBody;
     public float speed = 5.0f;
-    // Use this for initialization
+    // Use this for initialization Hi
     void Start () {
         rigidBody = GetComponent<Rigidbody2D>();
         rigidBody.velocity = new Vector2(-5.0f, 0.0f);
