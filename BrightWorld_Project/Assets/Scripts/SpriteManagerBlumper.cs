421ebdec3e05ed2e65ba83098d065788f4c1761b
Jon Shoberg <shobe028@umn.edu>
2016-08-07 09:15:27 -0500

    Animation changes. Unfucked things by restoring a backup

--- BrightWorld_Project/Assets/Scripts/SpriteManagerBlumper.cs
+++ BrightWorld_Project/Assets/Scripts/SpriteManagerBlumper.cs
@@ -3,17 +3,31 @@ using System.Collections;
 
 public class SpriteManagerBlumper : MonoBehaviour {
 
+	public Sprite friendlySprite;
+	public Sprite enemySprite;
+	private bool isFriendly;
+
 	// Use this for initialization
 	void Start () {
-	
+		isFriendly = true;
 	}
 
 	void Awake(){
 		DontDestroyOnLoad (this);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (LightManager.isDark && isFriendly) {
+			GetComponent<SpriteRenderer>().sprite = enemySprite;
+			isFriendly = false;
+			gameObject.layer = 13;
+		} else {
+			if (!LightManager.isDark && !isFriendly) {
+				GetComponent<SpriteRenderer>().sprite = friendlySprite;
+				isFriendly = true;
+				gameObject.layer = 9;
+			}
+		}
 	}
 }
--- BrightWorld_Project/Assets/Scripts/SpriteManagerPuffirhana.cs
+++ BrightWorld_Project/Assets/Scripts/SpriteManagerPuffirhana.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class SpriteManagerPuffirhana : MonoBehaviour {
 
+	public Sprite friendlySprite;
+	public Sprite enemySprite;
+	private bool isFriendly;
+
+	private Animator anim;
+
 	// Use this for initialization
 	void Start () {
-	
+		isFriendly = true;
+		anim = GetComponent<Animator> ();
 	}
 
 	void Awake(){
@@ -14,6 +21,16 @@ public class SpriteManagerPuffirhana : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (LightManager.isDark && isFriendly) {
+			anim.SetTrigger("Dark");
+			isFriendly = false;
+			gameObject.layer = 13;
+		} else {
+			if (!LightManager.isDark && !isFriendly) {
+				anim.SetTrigger("Light");
+				isFriendly = true;
+				gameObject.layer = 9;
+			}
+		}
 	}
 }
