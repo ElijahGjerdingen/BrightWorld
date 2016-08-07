a337dce92b6f412f27a129d56f3a2d529e4090c1
Jon Shoberg <shobe028@umn.edu>
2016-08-07 05:31:05 -0500

    Health bar do things

--- BrightWorld_Project/Assets/Scripts/BarUpdate.cs
+++ BrightWorld_Project/Assets/Scripts/BarUpdate.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class BarUpdate : MonoBehaviour {
 
+	public UnityEngine.UI.Slider healthBarSlider;
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		healthBarSlider.value = (float) LightManager.light / 100f;
 	}
 }
